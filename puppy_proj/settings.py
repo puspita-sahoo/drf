9fcaad4e016c44638b9337ffdbf2322af4908a07
puspita-sahoo <puspita.private@gmail.com>
2021-09-23 20:20:33 +0530

    added static

--- puppy_proj/settings.py
+++ puppy_proj/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.join(BASE_DIR, 'staticfiles')
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
