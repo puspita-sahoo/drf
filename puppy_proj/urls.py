5f642a4e5154decd0e4e3a1cae0e282b5f8ed75f
puspita-sahoo <puspita.private@gmail.com>
2021-09-23 20:17:57 +0530

    added static

--- puppy_proj/urls.py
+++ puppy_proj/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include,path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 
@@ -8,5 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('puppies_app.urls')),
     path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
